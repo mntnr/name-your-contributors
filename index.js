7882f97e581c37ebb8059f8aeef653383e902b4c
Richard Littauer <richard.littauer@gmail.com>
2016-01-17 05:57:46 +0100

    Cleaned up

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const Promise = require('bluebird')
 const _ = require('lodash')
 const getCodeReviewers = require('get-code-reviewers')
 const getIssueCommenters = require('get-issue-commenters')
-const getCommitters = require('get-committers')
+const getIssueCreators = require('get-github-issue-creators')
+const getGithubUser = require('get-github-user')
+const sortAlphabetically = require('sort-alphabetic')
 
-module.exports = function (organization, opts) {
-  Promise.join(
-    getCodeReviewers(opts),
-    getIssueCommenters(opts),
-    getCommitters(opts),
-    function (reviewers, commenters, committers) {
-      var union = _.union(reviewers, commenters, committers)
+module.exports = function (org, opts) {
+  return Promise.join(
+    getCodeReviewers({
+      org: org,
+      since: opts.since
+    }),
+    getIssueCommenters(org, opts),
+    getIssueCreators(org, opts),
+    function (reviewers, commenters, creators) {
+      var union = _.union(reviewers, commenters, creators)
       return union
     }
-  ).map(function (response) {
-    console.log(response)
-    // TODO Get information from Octocat about users
-    //console.log(`[@${commit.author.login}](//github.com/${commit.author.login}) (${commit.commit.author.name})`)
-  })
+  ).map(user => getGithubUser(user))
+  .then(_.flatten.bind(_))
+  .map((user) => {
+    var str = '[@' + user.login + '](//github.com/' + user.login + ')'
+    if (user.name)
+      str += ' (' + user.name + ')'
+    return str
+  }).then(arr => sortAlphabetically(arr))
 }
