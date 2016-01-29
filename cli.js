c2f031d7a3386d1ebb71137a502bc3ab2187e3a6
Richard Littauer <richard.littauer@gmail.com>
2016-01-28 19:13:23 -0500

    Added repository option

--- cli.js
+++ cli.js
@@ -8,15 +8,22 @@ const gitconfig = require('gitconfiglocal')
 const pify = require('pify')
 
 const cli = meow([`
-	Usage
-	  $ name-your-contributors <input> [opts]
+  Usage
+    $ name-your-contributors <input> [opts]
 
-	Examples
-	  $ name-your-contributors ipfs --since=2016-01-15T00:20:24Z
-	  [@RichardLitt](//github.com/RichardLitt) (Richard Littauer)
+  Options
+    -s, --since Add a time since
+    -u, --until Add a time to
+    -r, --repo A repository to search
+
+  Examples
+    $ name-your-contributors ipfs --since=2016-01-15T00:20:24Z --until=2016-01-20T00:20:24Z
+    [@RichardLitt](//github.com/RichardLitt) (Richard Littauer)
 `, {
   alias: {
-    s: 'since'
+    s: 'since',
+    u: 'until',
+    r: 'repo'
   }
 }])
 
