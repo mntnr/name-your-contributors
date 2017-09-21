6aaf7523f7a53b4c4212a04d9bf6d0dc7374231e
Thomas Getgood <thomas.getgood@gmail.com>
2017-09-21 13:03:59 -0400

    Fixed up CLI help comment.

--- src/cli.js
+++ src/cli.js
@@ -6,41 +6,35 @@ const graphql = require('./graphql');
 const queries = require('./queries');
 
 const cli = meow([`
-	Usage
-		$ name-your-contributors <input> [opts]
+  Usage
+    $ name-your-contributors <input> [opts]
 
-	Options
-		-s, --since Add a time since
-		-u, --until Add a time to
-		-r, --repo A repository to search
+  Options
+    -a, --after  - Get contributions after date
+    -b, --before - Get contributions before data
+    -r, --repo   - Repository to search
+    -u, --user   - User to which repository belongs
 
-	Examples
-		$ name-your-contributors ipfs --since=2016-01-15T00:20:24Z --until=2016-01-20T00:20:24Z
-		[@RichardLitt](//github.com/RichardLitt) (Richard Littauer)
+  Examples
+    $ name-your-contributors -r ipfs -u ipfs --after=2016-01-15T00:20:24Z --before=2016-01-20T00:20:24Z
+    [@RichardLitt](//github.com/RichardLitt) (Richard Littauer)
 `, {
-	alias: {
-		b: 'before',
-		a: 'after',
-		r: 'repo',
-		u: 'user'
-	}
+  alias: {
+    b: 'before',
+    a: 'after',
+    r: 'repo',
+    u: 'user'
+  }
 }])
 
 const token = process.env.GITHUB_TOKEN;
 
-// if (cli.flags.u && cli.flags.r && token) {
-//	graphql.executequery(token, queries.everything(cli.flags.r, cli.flags.u)).
-//		then(JSON.parse).
-//		then(queries.cleanData).
-//		then(console.log);
-// } else {
-//	console.error('You must currently specify both a user and a repo name. And provide a token.');
-//	process.exit(1);
-// }
-
-var x = {a:46}
-
-const y = x
-.a
-
-console.log(y)
+if (cli.flags.u && cli.flags.r && token) {
+  graphql.executequery(token, queries.everything(cli.flags.r, cli.flags.u)).
+    then(JSON.parse).
+    then(queries.cleanData).
+    then(console.log);
+} else {
+  console.error('You must currently specify both a user and a repo name. And provide a token.');
+  process.exit(1);
+}
